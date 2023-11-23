e40643cd87fdbe3465292d1670824217496dd532
Uzerkhan786 <uzer.khan2020@vitbhopal.ac.in>
2023-11-23 12:04:00 +0530

    adding express rate limit in which we do rate limiting

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express=require('express');
+const rateLimit=require('express-rate-limit');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app=express();
+const rate=rateLimit({
+    windowMs:15*60*1000,
+    limit:5
 
+})
+app.use(rate);
 app.use('/bookingservice',createProxyMiddleware({target:'http://localhost:3002',changeOrigin:true}))
 
-app.get('/home',async(req,res)=>{
-    res.json({
-        data:'hi'
-    })
-})
 app.listen(3000,()=>{
     console.log('something is runnin on port 3000');
 })
